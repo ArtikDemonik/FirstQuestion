91b1eaba2c80f7103ab4d7105e345035f844126d
ArtikDemonik <61469976+ArtikDemonik@users.noreply.github.com>
2023-01-27 09:56:03 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,10 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Type x (x in [-1;1]:");
         double x = in.nextDouble();
+        if(-1>x>1){
+            System.out.println("x not in the range");
+            return;
+        }
         System.out.println("Type n:");
         double n = in.nextDouble();
         double sum = n/2;
