eee64c8156c9d9587d7ef5c6d43070f28690cb22
ArtikDemonik <artikdemonik@yandex.ru>
2023-01-27 09:31:15 +0300

    Add all code

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type x (x in [-1;1]:");
+        double x = in.nextDouble();
+        System.out.println("Type n:");
+        double n = in.nextDouble();
+        double sum = n/2;
+        for(int i = 0; i <= n ; i++){
+            sum += (pow(-1, i+1)+pow(x, 2*i+1))/(2*i+1);
+        }
+        System.out.printf("Result: %f", sum);
+    }
+}
